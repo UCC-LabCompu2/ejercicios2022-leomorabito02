3612dfc8895c9fcce66fdbdbba50d4cdf71257f2
Leonardo Morabito <leonardomorabito02@gmail.com>
2022-05-18 23:54:06 -0300

    comienzo canvas

---
+++ _ejerciciosLeo/JS/ej_canva_circulo_cuadrado/misFunciones.js
@@ -0,0 +1,13 @@
+/**
+ * Descripción
+ * @method Nombre de la función
+ * @param Parámetro A
+ * @param Parámetro B
+ * @return Valor que retorna
+ */
+function dibujar() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+
+    //terminar
+}
