a4e1d582fd31ab00527c56b39ee5c66a24fa49fc
Leonardo Morabito <leonardomorabito02@gmail.com>
2022-05-18 22:04:20 -0300

    ejercicios js & refactoreo el conversor de unidades

--- _ejerciciosLeo/JS/ej_conversorUnidades y documentacion/function.js
+++ _ejerciciosLeo/JS/ej_conversorUnidades y documentacion/function.js
@@ -5,23 +5,31 @@
  * @param  {number} valor - El valor de los inputs de metros, yardas, pies o pulgadas
  */
 function cambiar_unidades(id, valor) {
+    var metro, pulgada, pie, yarda;
     if (isNaN(valor)) {
-        alert("Se ingresó un valor invalido"+id);
-        document.Unidades.unid_metro.value = "";
-        document.Unidades.unid_pulgada.value = "";
-        document.Unidades.unid_pie.value = "";
-        document.Unidades.unid_yarda.value = "";
+        alert("Se ingresó un valor invalido" + id);
+        metro = "";
+        pulgada = "";
+        pie = "";
+        yarda = "";
     } else if (id == "metro") {
-        document.Unidades.unid_pulgada.value = 39.3701 * valor;
-        document.Unidades.unid_pie.value = 3.28084 * valor;
-        document.Unidades.unid_yarda.value = 1.03961 * valor;
+        metro = valor;
+        pulgada = 39.3701 * valor;
+        pie = 3.28084 * valor;
+        yarda = 1.03961 * valor;
     } else if (id == "pulgada") {
-        document.Unidades.unid_metro.value = 0.0254 * valor;
-        document.Unidades.unid_pie.value = 0.0833333 * valor;
-        document.Unidades.unid_yarda.value = 0.0277778 * valor;
+        pulgada = valor;
+        metro = 0.0254 * valor;
+        pie = 0.0833333 * valor;
+        yarda = 0.0277778 * valor;
     } else if (id == "yarda") {
-        document.Unidades.unid_metro.value = 0.9144 * valor;
-        document.Unidades.unid_pulgada.value = 36 * valor;
-        document.Unidades.unid_pie.value = 3 * valor;
+        yarda = valor;
+        metro = 0.9144 * valor;
+        pulgada = 36 * valor;
+        pie = 3 * valor;
     }
+    document.Unidades.unid_metro.value = metro;
+    document.Unidades.unid_pulgada.value = pulgada;
+    document.Unidades.unid_pie.value = pie;
+    document.Unidades.unid_yarda.value = yarda;
 }
